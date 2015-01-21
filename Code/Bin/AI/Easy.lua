bd59c3fda6bc798e88dce5424124335a0537bb93
Liaozengxiang <Liaozengxiang@users.noreply.github.com>
2015-01-22 00:23:36 +0800

    产品名称：Tank 修改内容： 1. 添加 LUA 脚本和关卡地图；

---
+++ Code/Bin/AI/Easy.lua
@@ -0,0 +1,12 @@
+-- 简单 AI 的实现
+-- Copyright (C) 2015. All rights reserved
+
+
+-- 新的关卡开始，由 C++ 程序调用
+-- 参数:
+--       nStageID  关卡编写
+function OnNewStage(nStageID)
+	print("Stage " .. tostring(nStageID) .. " started");
+end
+
+OnNewStage(1);
