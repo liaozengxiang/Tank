542f7302a8bd4bb5db2f82c232998c8f0e92036a
Liaozengxiang <Liaozengxiang@users.noreply.github.com>
2015-01-22 01:04:13 +0800

    产品名称：Tank 修改内容： 1. 引入 hge181 引擎；

--- Code/Source/Startup/Main.cpp
+++ Code/Source/Startup/Main.cpp
@@ -1,6 +1,12 @@
 #include <windows.h>
+#include "../hge181/include/hge.h"
+
+// Lib 库的链接路径是以工程为相对路径进行查找的
+#pragma comment(lib, "../Source/hge181/lib/vc/hge.lib")
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
+	HGE *pHGE = hgeCreate(HGE_VERSION);
+
 	return 0;
 }
